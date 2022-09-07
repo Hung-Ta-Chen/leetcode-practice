482c1049f6b53f6182d708c13a5a696f214689ef
Hung-Ta-Chen <112988200+Hung-Ta-Chen@users.noreply.github.com>
2022-09-07 03:30:14 -0700

    Add comment

--- 1_2sum_hashtable.cpp
+++ 1_2sum_hashtable.cpp
@@ -5,9 +5,10 @@ public:
         vector<int> ans;
         
         for(int i = 0; i < nums.size(); i++){
-            if (table.find(target - nums[i]) == table.end())
-                table.insert(pair<int, int>(nums[i], i));
-            else{
+            if (table.find(target - nums[i]) == table.end())   // map.find(key) == map.end() if key not found
+                table.insert(pair<int, int>(nums[i], i));    // if the complement number is not in hash table yet, insert it into the table
+            else{ 
+                // if find the complement, return the indices
                 ans.push_back(table[target - nums[i]]);
                 ans.push_back(i);
                 return ans;
