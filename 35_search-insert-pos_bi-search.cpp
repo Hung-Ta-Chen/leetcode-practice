0f5199c8ec0a3141bf85a728b9558f59f4fede7f
Hung-Ta Chen <htcchen@ucdavis.edu>
2022-11-20 22:36:24 -0800

    Update 35_search-insert-pos_bi-search.cpp

--- 35_search-insert-pos_bi-search.cpp
+++ 35_search-insert-pos_bi-search.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {       
+    int searchInsert(vector<int>& nums, int target) {
         if (target <= nums[0]) return 0;
         if (target > nums[nums.size()-1]) return nums.size();
         
-        int head = 0;
-        int tail = nums.size() - 1;
-        int m = head + (tail - head + 1) / 2;
-        
-        while(target <= nums[m-1] || target > nums[m]){  // Binary search
-            if (target <= nums[m-1])
-                tail = m - 1;
+        int low = 0, high = nums.size()-1, mid;
+        while(low <= high){
+            mid = (low + high) / 2;   
+            if(nums[mid] == target)
+                // Find identical element, insert here
+                return mid;
+            else if(nums[mid] < target)
+                low = mid + 1;
             else
-                head = m;
-            
-            m = head + (tail - head + 1) / 2;             
+                high = mid -1;           
         }
-        return m;
+        // No idential element in the vector
+        // low = high + 1
+        // now low points to the element which is the successor of target
+        return low;
     }
 };
