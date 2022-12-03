6fcae49c27b8b724107a49747fb8460822ad7786
Hung-Ta Chen <htcchen@ucdavis.edu>
2022-12-03 01:42:38 -0800

    Update 26_RemoveDuplicatesFromSortedArr.cpp

--- 26_RemoveDuplicatesFromSortedArr.cpp
+++ 26_RemoveDuplicatesFromSortedArr.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int>::iterator iter = nums.begin();
-        
-        int first = -101;
-        while(iter != nums.end()){
-            if (*iter != first){
-                first = *iter;
-                iter++;
-            }
-            else{
-                // 當一vector的一element被erase,後面剩下的elements會往前補上memory
-                // 因此erase之後iter不用往後移
-                nums.erase(iter);
-            }
-        }        
-        return nums.size();        
+        // We only have to put all k unique values in hte first k elements
+        int unique = 1;  // "unique" keeps track of num of unique values found
+        for(int i = 1; i < nums.size(); i++)
+            if(nums[i] != nums[i-1]) nums[unique++] = nums[i];
+        return unique;
     }
 };
