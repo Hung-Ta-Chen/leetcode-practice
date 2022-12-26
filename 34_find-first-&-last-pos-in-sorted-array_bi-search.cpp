3dc294e94b855db02d7af6a51adc5b51cd114403
Hung-Ta Chen <htcchen@ucdavis.edu>
2022-12-25 17:43:34 -0800

    Update 34_find-first-&-last-pos-in-sorted-array_bi-search.cpp

--- 34_find-first-&-last-pos-in-sorted-array_bi-search.cpp
+++ 34_find-first-&-last-pos-in-sorted-array_bi-search.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> pos(2, -1);
-        if(nums.size() == 0) return pos;
-        
-        int low = 0, high = nums.size()-1, mid;
-        // Find the first position first
-        while(low <= high){
-            mid = (low + high) / 2;
-            if(nums[mid] == target){
-                // If the match is at the head of array
-                if(mid == 0){
-                    pos[0] = mid;
-                    break;
-                }                    
-                else{
-                    // If the previous element of match doesn't match
-                    if(nums[mid-1] != target){
-                        pos[0] = mid;
-                        break;
-                    }            
-                    else
-                        high = mid - 1;
-                }
-            }
-            else if(nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;       
-        }
-        
-        // Find the last position
+        int low, high, mid;
+        vector<int> ans(2, -1);
+        // find start
         low = 0, high = nums.size()-1;
         while(low <= high){
             mid = (low + high) / 2;
-            if(nums[mid] == target){
-                // If the match is at the tail of array
-                if(mid == nums.size()-1){
-                    pos[1] = mid;
+            if(nums[mid] < target) low = mid + 1;
+            else if(nums[mid] > target) high = mid - 1;
+            else{
+                if(mid == 0 || nums[mid-1] != target){
+                    ans[0] = mid;
                     break;
-                }                   
-                else{
-                    // If the following element of match doesn't match
-                    if(nums[mid+1] != target){
-                        pos[1] = mid;
-                        break;
-                    }                  
-                    else
-                        low = mid + 1;
                 }
+                else high = mid - 1;
             }
-            else if(nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;       
-        }        
-        return pos;
+        }       
+        // find end
+        low = 0, high = nums.size()-1;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] > target) high = mid - 1;
+            else if(nums[mid] < target)low = mid + 1;
+            else{
+                if(mid == nums.size()-1 || nums[mid+1] != target){
+                    ans[1] = mid;
+                    break;
+                } 
+                else low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
