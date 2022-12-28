17c30b37cbc586dd9b8588e78163d2c73a8416c1
Hung-Ta Chen <htcchen@ucdavis.edu>
2022-12-28 04:13:01 -0800

    Update 3_longest-substr-wo-repeat-char_sliding-win_2.cpp

--- 3_longest-substr-wo-repeat-char_sliding-win_2.cpp
+++ 3_longest-substr-wo-repeat-char_sliding-win_2.cpp
@@ -8,7 +8,7 @@ public:
         int max_len = 0;
         int left, right;
         for(left = 0, right = 0; right < s.size(); right++){
-            // We need max here because we don't want left to go backward when encountering a previously recorded character
+            // We need max here because we don't want left to go backward when encountering a previously recorded character (consider the case "abba")
             if(char_pos.find(s[right]) != char_pos.end()) left = max(left, char_pos[s[right]] + 1);
             max_len = max(max_len, right - left + 1);
             char_pos[s[right]] = right;
