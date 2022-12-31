7a08c825fd489fabe8adb7a06e67489909b44b6f
Hung-Ta Chen <htcchen@ucdavis.edu>
2022-12-31 01:21:39 -0800

    Update 1091_shortest-path-in-binary-matrix_bfs.cpp

--- 1091_shortest-path-in-binary-matrix_bfs.cpp
+++ 1091_shortest-path-in-binary-matrix_bfs.cpp
@@ -5,21 +5,21 @@ public:
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
         int path_len = 0;
-        # BFS
+        // BFS
         queue<pair<int, int>> q;
         q.push(make_pair(0, 0));
         visited[0][0] = true;
         while(!q.empty()){
-            # Maintain the size of queue as the size of level for better time complexity
+            // Maintain the size of queue as the size of level for better time complexity
             int lvl_size = q.size();
             path_len++;
             for(int k = 0; k < lvl_size; k++){
                 int i = q.front().first;
                 int j = q.front().second;
                 q.pop();
-                # Reach the endpoint
+                // Reach the endpoint
                 if(i == m-1 && j == n-1) return path_len; 
-                # Push the unvisited neighbors into the queue
+                // Push the unvisited neighbors into the queue
                 if(i != 0 && j != 0 && grid[i-1][j-1] == 0 && !visited[i-1][j-1]){
                     q.push(make_pair(i-1, j-1));
                     visited[i-1][j-1] = true;
