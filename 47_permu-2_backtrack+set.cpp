f2bdf6ffe1610f463d7d31c4c2b205b5d09e2fe5
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-03 23:11:51 -0800

    Update and rename 47_unique-permu_recur+set.cpp to 47_permu-2_backtrack+set.cpp

--- 47_permu-2_backtrack+set.cpp
+++ 47_permu-2_backtrack+set.cpp
@@ -1,3 +1,13 @@
+/*
+Note 1:
+We cannot avoid the repeated element by sorting the array at the beginning,
+since we swap two elements in each recursion, making the array unsorted.
+
+Note 2:
+We don't actually need a array to record the permutation,
+since any permutation has the exact same size as 'nums',
+so we just need to swaap the elements of 'nums'
+*/
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
@@ -7,14 +17,12 @@ public:
     }
     
     void swap_two_elements(vector<int>& nums, int low, vector<vector<int>>& perm){
-        if(low == nums.size()-1)
-            perm.push_back(nums);
+        if(low == nums.size()-1) perm.push_back(nums);
         else{
             // Use set to check repeated element
             set<int> repeat;
             for(int i = low; i < nums.size(); i++){
-                if(repeat.find(nums[i]) != repeat.end())
-                    continue;
+                if(repeat.find(nums[i]) != repeat.end()) continue;
                 else{
                     repeat.insert(nums[i]);
                     // Swap the lowest element and i-th element for further permutaton
