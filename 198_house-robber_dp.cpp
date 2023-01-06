edbd709e30b3842b55da0e854dc9fbf24ef5cb7b
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-05 21:18:30 -0800

    Update 198_house-robber_dp.cpp

--- 198_house-robber_dp.cpp
+++ 198_house-robber_dp.cpp
@@ -1,5 +1,19 @@
 /*
 We can also start from 0 houses (dp[0]=0) for better generalization
+
+####################################################################################
+int rob(vector<int>& nums) {
+    if(nums.size() == 1) return nums[0];
+    vector<int> dp(nums.size()+1, 0);  // index i => tot amount for first i houses
+    dp[0] = 0;
+    dp[1] = nums[0];
+    for(int i = 2; i <= nums.size(); i++){
+        dp[i] = max(dp[i-2]+nums[i-1], dp[i-1]);
+    }
+    return dp[nums.size()];
+}
+####################################################################################
+
 */
 class Solution {
 public:
