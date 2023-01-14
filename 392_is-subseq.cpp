d3930842c448c7b2143def90af5bb3161cd26d6a
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-14 01:08:57 -0800

    Update and rename 392_IsSubseq.cpp to 392_is-subseq.cpp

--- 392_is-subseq.cpp
+++ 392_is-subseq.cpp
@@ -3,10 +3,8 @@ public:
     bool isSubsequence(string s, string t) {
         if(s.size() == 0) return true;
         if(t.size() == 0) return false;
-        if(s.size() > t.size()) return false;
-        
-        int idx_s = 0, idx_t = 0;
-        
+        if(s.size() > t.size()) return false;        
+        int idx_s = 0, idx_t = 0;     
         //Iterate through the string t
         while(idx_t < t.size()){
             //If find a match to current char in string s, go to the next char in s
