5f84925a76659dd97ca2e78999f14d1f2c5d749c
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-20 00:46:46 -0800

    Create 704_binary-search_bisect.py

---
+++ 704_binary-search_bisect.py
@@ -0,0 +1,5 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        idx = bisect.bisect_left(nums, target)
+        if idx == len(nums) or nums[idx] != target: return -1
+        else: return idx
