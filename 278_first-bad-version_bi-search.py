b1926ece3d57465a3e784541da7f859f156d6fd3
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-26 13:53:41 -0800

    Update 278_first-bad-version_bi-search.py

--- 278_first-bad-version_bi-search.py
+++ 278_first-bad-version_bi-search.py
@@ -7,7 +7,9 @@ class Solution:
         while low < high:
             mid = (low + high) // 2
             if isBadVersion(mid):
+                # Our candidate will still include mid
                 high = mid
             else:
+                # Our candidate won't include mid
                 low = mid + 1
         return low
