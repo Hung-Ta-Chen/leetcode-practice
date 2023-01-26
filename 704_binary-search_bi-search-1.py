e31ddd3fe3b1d129ef90b2171393679a237c326a
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-01-26 13:03:57 -0800

    Update 704_binary-search_bi-search-1.py

--- 704_binary-search_bi-search-1.py
+++ 704_binary-search_bi-search-1.py
@@ -1,6 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low = 0; high = len(nums)
+        low = 0; high = len(nums)-1
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
